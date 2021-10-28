ae9c19734340aab1c276d31217f9e6cb6f2ae2ee
aulneau <aulneau@users.noreply.github.com>
2021-10-27 19:13:07 -0500

    chore: cors

--- pages/api/votes.ts
+++ pages/api/votes.ts
@@ -1,7 +1,28 @@
+import Cors from 'cors';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getVoteData } from '@fungible-systems/sip-12';
 
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await runMiddleware(req, res, cors);
   res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate');
   res.status(200).json(await getVoteData());
 }

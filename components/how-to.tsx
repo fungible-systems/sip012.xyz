2ca837543ce082d5d943f299bc330c4f063adce8
asteria <asteria@syvita.org>
2021-10-31 16:38:52 +0000

    how to vote

--- components/how-to.tsx
+++ components/how-to.tsx
@@ -49,6 +49,37 @@ export const HowTo = () => {
           .
         </Text>
         <Text maxWidth="72ch">
+          You can use Electrum Wallet to send Bitcoin to the above addresses. This assumes you have an "SP" Stacks address and have stacked independently. If you stacked in a pool, make sure your pool admin votes using the following tutorial. Here's how to do it:
+        </Text>
+        <Text maxWidth="72ch">
+          <br />
+          1. Download Electrum Wallet from <Link
+            href={
+              'https://github.com/bitcoin/bitcoin/blob/6d8543504d8c5bde1d12a3c60407dee44d2c8e11/src/policy/policy.cpp#L16-L29'
+            }
+          >
+            here
+          </Link>.
+          <br />
+          2. Open Electrum Wallet.
+          <br />
+          3. Create a new standard wallet.
+          <br />
+          4. Choose "I already have a seed"
+          <br />
+          5. Enter your Stacks wallet seed. and change the seed type in options to BIP39
+          <br />
+          6. Click "Next" and change your wallet type to "legacy" and override the derivation path to (m/44'/5757'/0'), without the brackets.
+          <br />
+          7. Enter a password and click "Next".
+          <br />
+          8. Go to the Receive tab and click "New Address".
+          <br />
+          9. Send ~6,000 sats of Bitcoin to the address you just created.
+          <br />
+          10. Once you have some Bitcoin on your address, go to the send tab and send 5000 sats to one of the addresses above.
+        </Text>
+        <Text maxWidth="72ch">
           The vote-tallying software will take it from there. Happy voting and thank you for your
           participation on the network!
         </Text>
